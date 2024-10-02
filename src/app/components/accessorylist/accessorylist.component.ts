aa5ad8cd0400f1924d2a3eb9dd0cb0cfe59f5d8e
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:18:03 +0800

    feat: add accessory list

---
+++ src/app/components/accessorylist/accessorylist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accessorylist',
+  templateUrl: './accessorylist.component.html',
+  styleUrl: './accessorylist.component.css'
+})
+export class AccessorylistComponent {
+  accessoryList: string[] = [];
+  accessoryName: string = '';
+
+  addAccessories() {
+    this.accessoryList.push(this.accessoryName);
+  }
+}
