65e15fab2321b82015e02fe0e405f9c98d5dec89
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:24:51 +0800

    feat: add car model list

---
+++ src/app/components/carmodellist/carmodellist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carmodellist',
+  templateUrl: './carmodellist.component.html',
+  styleUrl: './carmodellist.component.css'
+})
+export class CarmodellistComponent {
+  carModelList: string[] = [];
+  carModelName: string = '';
+
+  addCarModels() {
+    this.carModelList.push(this.carModelName);
+  }
+}
