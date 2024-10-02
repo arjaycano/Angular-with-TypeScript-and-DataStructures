2950b21ae239c68963cb8ae27f503490d69c4f96
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:50:57 +0800

    feat: add product list

---
+++ src/app/components/productlist/productlist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productlist',
+  templateUrl: './productlist.component.html',
+  styleUrl: './productlist.component.css'
+})
+export class ProductlistComponent {
+  productList: string[] = [];
+  productName: string = '';
+
+  addProducts() {
+    this.productList.push(this.productName);
+  }
+}
