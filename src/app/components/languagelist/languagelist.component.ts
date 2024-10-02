a92029b88219d1f98040e426c16a7db4340c197f
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:40:33 +0800

    feat: add language list

---
+++ src/app/components/languagelist/languagelist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-languagelist',
+  templateUrl: './languagelist.component.html',
+  styleUrl: './languagelist.component.css'
+})
+export class LanguagelistComponent {
+  languageList: string[] = [];
+  languageName: string = '';
+
+  addLanguages() {
+    this.languageList.push(this.languageName);
+  }
+}
