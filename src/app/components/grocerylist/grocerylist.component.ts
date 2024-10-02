742abad46f80a735bf3c97c72f035cead7b4cbdc
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:38:54 +0800

    feat: add grocery list

---
+++ src/app/components/grocerylist/grocerylist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grocerylist',
+  templateUrl: './grocerylist.component.html',
+  styleUrl: './grocerylist.component.css'
+})
+export class GrocerylistComponent {
+  groceryList: string[] = [];
+  groceryName: string = '';
+
+  addGroceries() {
+    this.groceryList.push(this.groceryName);
+  }
+}
