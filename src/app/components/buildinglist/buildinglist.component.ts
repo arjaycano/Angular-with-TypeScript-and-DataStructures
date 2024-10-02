76135c9b9fa914d57b4252c9cadb4cf861d77cb6
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:24:00 +0800

    feat: add building list

---
+++ src/app/components/buildinglist/buildinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buildinglist',
+  templateUrl: './buildinglist.component.html',
+  styleUrl: './buildinglist.component.css'
+})
+export class BuildinglistComponent {
+  buildingList: string[] = [];
+  buildingName: string = '';
+
+  addBuildings() {
+    this.buildingList.push(this.buildingName);
+  }
+}
