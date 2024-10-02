b42b8e99104c46fd31b52070f4dbecc493c8fe8a
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:27:21 +0800

    feat: add composer list

---
+++ src/app/components/composerlist/composerlist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-composerlist',
+  templateUrl: './composerlist.component.html',
+  styleUrl: './composerlist.component.css'
+})
+export class ComposerlistComponent {
+  composerList: string[] = [];
+  composerName: string = '';
+
+  addComposers() {
+    this.composerList.push(this.composerName);
+  }
+}
