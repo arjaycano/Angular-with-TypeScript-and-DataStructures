bede483bc31d8f20e5de093952c59973da12eb88
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:19:54 +0800

    feat: add animal list

---
+++ src/app/components/animallist/animallist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animallist',
+  templateUrl: './animallist.component.html',
+  styleUrl: './animallist.component.css'
+})
+export class AnimallistComponent {
+  animalList: string[] = [];
+  animalName: string = '';
+
+  addAnimals() {
+    this.animalList.push(this.animalName);
+  }
+}
