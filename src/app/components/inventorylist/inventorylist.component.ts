ab1c1272e8fa94f997740cce5da1e6923dbf2ba6
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:39:40 +0800

    feat: add inventory list

---
+++ src/app/components/inventorylist/inventorylist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventorylist',
+  templateUrl: './inventorylist.component.html',
+  styleUrl: './inventorylist.component.css'
+})
+export class InventorylistComponent {
+  inventoryList: string[] = [];
+  inventoryName: string = '';
+
+  addInventories() {
+    this.inventoryList.push(this.inventoryName);
+  }
+}
