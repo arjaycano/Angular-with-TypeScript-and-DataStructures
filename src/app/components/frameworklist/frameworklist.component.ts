c3c0738094cc3f91ee00b053cd4863211b8921fc
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:36:06 +0800

    feat: add framework list

---
+++ src/app/components/frameworklist/frameworklist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frameworklist',
+  templateUrl: './frameworklist.component.html',
+  styleUrl: './frameworklist.component.css'
+})
+export class FrameworklistComponent {
+  frameworkList: string[] = [];
+  frameworkName: string = '';
+
+  addFrameworks() {
+    this.frameworkList.push(this.frameworkName);
+  }
+}
