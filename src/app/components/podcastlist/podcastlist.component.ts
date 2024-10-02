ce0a1dffc90678fa5ab0ce84c7b3b8c72021c2a3
arjaycano <canoroerenzjoseph@gmail.com>
2024-10-02 22:49:27 +0800

    feat: add podcast list

---
+++ src/app/components/podcastlist/podcastlist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcastlist',
+  templateUrl: './podcastlist.component.html',
+  styleUrl: './podcastlist.component.css'
+})
+export class PodcastlistComponent {
+  podcastList: string[] = [];
+  podcastName: string = '';
+
+  addPodcasts() {
+    this.podcastList.push(this.podcastName);
+  }
+}
